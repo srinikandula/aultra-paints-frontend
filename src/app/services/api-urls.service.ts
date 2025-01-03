6d476ce4149a1227f6a9213578ba63e4beeca69b
SravanKumar-G <sravang432@gmail.com>
2025-01-03 17:50:36 +0530

    api url changes

--- src/app/services/api-urls.service.ts
+++ src/app/services/api-urls.service.ts
@@ -9,7 +9,7 @@ export class ApiUrlsService {
     }
 
     // mainUrl = "http://localhost:4300/api/";
-    mainUrl = "http://paintsapi.ultrapaints.in/api/";
+    mainUrl = "http://api.aultrapaints.com/api/";
 
     // Users API URLs
     getUsers = 'users/all';
