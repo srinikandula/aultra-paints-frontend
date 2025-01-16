d0e2eacede22d2e13ee4b9635add8c6db745aa9d
PuttaSurya <suryapavan7a77@gmail.com>
2025-01-15 16:54:53 -0800

    userpaginationandenviormentfileschanges

--- src/app/services/api-urls.service.ts
+++ src/app/services/api-urls.service.ts
@@ -1,72 +1,71 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class ApiUrlsService {
 
-    constructor() {
-    }
+  constructor() { }
 
-    // mainUrl = "http://localhost:4300/api/";
-    mainUrl = "https://api.aultrapaints.com/api/";
+  // Use dynamic URL from environment file
+  mainUrl = environment.apiUrl;
 
-    // Users API URLs
-    getUsers = 'users/all';
-    addUser = 'users/add';
-    updateUser = 'users';
-    deleteUser = 'users';
-    searchUser = 'users/searchUser';
-    toggleUserStatus = 'users/toggle-status';
-    resetPassword = 'users/resetPassword';
-    userDashboard = 'users/userDashboard';
+  // Users API URLs
+  getUsers = 'users/all';
+  addUser = 'users/add';
+  updateUser = 'users';
+  deleteUser = 'users';
+  searchUser = 'users/searchUser';
+  toggleUserStatus = 'users/toggle-status';
+  resetPassword = 'users/resetPassword';
+  userDashboard = 'users/userDashboard';
 
-    // API Urls
-    register = 'auth/register';
+  // API Urls
+  register = 'auth/register';
 
-    // batchApiUrls
-    createBatch = 'batchNumbers/add'
-    getBatches = 'batchNumbers';
-    searchBranch = 'batchNumbers/';
-    couponSeries = 'batchNumbers/couponSeries';
-    updateBatch = 'batchNumbers/update/';
-    deleteBatch = 'batchNumbers/delete/';
-    branchDeletedAffectedCouponsCount = 'batchNumbers/branchDeletedAffectedCouponsCount/'
+  // batchApiUrls
+  createBatch = 'batchNumbers/add';
+  getBatches = 'batchNumbers';
+  searchBranch = 'batchNumbers/';
+  couponSeries = 'batchNumbers/couponSeries';
+  updateBatch = 'batchNumbers/update/';
+  deleteBatch = 'batchNumbers/delete/';
+  branchDeletedAffectedCouponsCount = 'batchNumbers/branchDeletedAffectedCouponsCount/';
 
-//   transactions
-    getTransactions = 'transaction';
+  // Transactions
+  getTransactions = 'transaction';
 
+  // Product URLs
+  createProduct = 'products';
+  getProducts = 'products';
+  getAllProducts = 'products/getAllProducts';
+  getProductByName = 'products/search'; 
+  updateProduct = 'products';
+  deleteProduct = 'products';
 
-    // Product URLs
-    createProduct = 'products';
-    getProducts = 'products';
-    getAllProducts = 'products/getAllProducts';
-    getProductByName = 'products/search'; 
-    updateProduct = 'products';
-    deleteProduct = 'products';
+  // API Urls for Brands
+  createBrand = 'brands';
+  getBrands = 'brands';
+  getBrandsByProductId = 'brands';
+  updateBrand = 'brands';
+  deleteBrand = 'brands';
+  getAllBrandsForSelect = 'brands/getAllBrandsForSelect/';
+  searchBrandsByName = 'brands/search';
 
-    // API Urls for Brands
-    createBrand = 'brands';
-    getBrands = 'brands';
-    getBrandsByProductId = 'brands';
-    updateBrand = 'brands';
-    deleteBrand = 'brands';
-    getAllBrandsForSelect = 'brands/getAllBrandsForSelect/'
-    searchBrandsByName = 'brands/search'
+  // API Urls for Product Offers
+  createProductOffer = 'productOffers/create';
+  searchProductOffers = 'productOffers/searchProductOffers';
+  getProductOffers = 'productOffers/getProductOffers';
+  getProductOfferById = 'productOffers/getProductOfferById/';
+  updateProductOffer = 'productOffers/update/';
+  deleteProductOffer = 'productOffers/delete/';
 
-    // API Urls for Product Offers
-    createProductOffer = 'productOffers/create';
-    searchProductOffers = 'productOffers/searchProductOffers'
-    getProductOffers = 'productOffers/getProductOffers';
-    getProductOfferById = 'productOffers/getProductOfferById/';
-    updateProductOffer = 'productOffers/update/';
-    deleteProductOffer = 'productOffers/delete/';
-
-    // API Urls for Reward Schemes
-    createRewardScheme = 'rewardSchemes/create';
-    searchRewardSchemes = 'rewardSchemes/searchRewardSchemes'
-    getRewardSchemes = 'rewardSchemes/getRewardSchemes';
-    getRewardSchemeById = 'rewardSchemes/getRewardSchemeById/';
-    updateRewardScheme = 'rewardSchemes/update/';
-    deleteRewardScheme = 'rewardSchemes/delete/';
+  // API Urls for Reward Schemes
+  createRewardScheme = 'rewardSchemes/create';
+  searchRewardSchemes = 'rewardSchemes/searchRewardSchemes';
+  getRewardSchemes = 'rewardSchemes/getRewardSchemes';
+  getRewardSchemeById = 'rewardSchemes/getRewardSchemeById/';
+  updateRewardScheme = 'rewardSchemes/update/';
+  deleteRewardScheme = 'rewardSchemes/delete/';
 }
